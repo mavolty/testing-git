79989d39bff2a5f2b96b34da2ce5b11ded2b3469
egawangga <egawangga@gmail.com>
2025-11-16 23:58:56 +1000

    tambahin code

--- init.py
+++ init.py
@@ -1,1 +1,13 @@
 import pandas as pd
+import numpy as np
+
+from xgboost import plot_tree
+
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+
+import os
+from os.path import join as pjoin
+
+print("hello")
